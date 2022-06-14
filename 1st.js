721989d6bfaf430efefc84f8cdf235f342643ace
peter adesanya <haryoroluwah429@gmail.com>
2022-06-14 13:23:20 +0100

    add work

---
+++ 1st.js
@@ -0,0 +1,11 @@
+let js = "amazing";
+console.log(40 + 8 + 23 -10);
+
+console.log("jonas");
+console.log(23);
+
+let firstname = "perolio";
+console.log(firstname);
+
+var peter = document.getElementById("bcknds");
+peter.style.backgroundColor = "pink";
